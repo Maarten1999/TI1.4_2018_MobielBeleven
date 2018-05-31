ecf156fd543abf5fd31d6f147a686bf1c6fc91f6
Unknown <renske1999@live.nl>
2018-05-31 10:21:31 +0200

    Puzzle screen

--- MobielBeleven/app/src/main/java/com/a5/mobielbeleven/Activities/MainActivity.java
+++ MobielBeleven/app/src/main/java/com/a5/mobielbeleven/Activities/MainActivity.java
@@ -1,6 +1,6 @@
 package com.a5.mobielbeleven.Activities;
 
-//import java.awt.*;
+import java.awt.*;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
