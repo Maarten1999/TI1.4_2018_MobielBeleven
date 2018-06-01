915a611722893d17fd1ff76fe1330bd294dc06eb
Unknown <maartp@hotmail.com>
2018-06-01 09:01:17 +0200

    added dialog to snakemenu to ask for name

--- MobielBeleven/app/src/main/java/com/a5/mobielbeleven/Activities/SnakeMenu.java
+++ MobielBeleven/app/src/main/java/com/a5/mobielbeleven/Activities/SnakeMenu.java
@@ -1,6 +1,8 @@
 package com.a5.mobielbeleven.Activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -9,16 +11,22 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.a5.mobielbeleven.R;
 
+import static android.text.InputType.TYPE_CLASS_TEXT;
+import static android.text.InputType.TYPE_TEXT_VARIATION_PERSON_NAME;
+
 public class SnakeMenu extends BaseToolbar
 {
     private TextView scoreBoard;
-    Button sendButton,newGameButton;
+    Button sendButton, newGameButton;
     private final int SNAKE_GAME_SCORE = 0;
 
     @Override
@@ -44,7 +52,7 @@ public class SnakeMenu extends BaseToolbar
                         getApplicationContext(),
                         Snake.class
                 );
-                startActivityForResult(intent,SNAKE_GAME_SCORE);
+                startActivityForResult(intent, SNAKE_GAME_SCORE);
             }
         });
 
@@ -55,8 +63,7 @@ public class SnakeMenu extends BaseToolbar
             @Override
             public void onClick(View view)
             {
-                Toast.makeText(getApplicationContext(),
-                        "Sended score", Toast.LENGTH_LONG).show();
+                askForName();
             }
         });
 
@@ -73,9 +80,10 @@ public class SnakeMenu extends BaseToolbar
         }
     }
 
-    private void setHighScoreBoard(int newScore){
+    private void setHighScoreBoard(int newScore)
+    {
         int score = PreferenceManager.getDefaultSharedPreferences(this).getInt("score", 0);
-        if(newScore > score){
+        if (newScore > score) {
 
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
             SharedPreferences.Editor editor = prefs.edit();
@@ -85,26 +93,67 @@ public class SnakeMenu extends BaseToolbar
             scoreBoard.setText(String.valueOf(newScore));
             sendButton.setVisibility(Button.VISIBLE);
             newGameButton.setText(R.string.snake_menu_button_improve);
-        }
-        else
-        {
+        } else {
             Toast.makeText(getApplicationContext(),
                     "Highscore niet verbeterd", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void checkScore(){
+    private void checkScore()
+    {
         int currentScore = PreferenceManager.getDefaultSharedPreferences(this).getInt("score", 0);
         if (currentScore != 0) {
             sendButton.setVisibility(Button.VISIBLE);
             newGameButton.setText(R.string.snake_menu_button_improve);
-        }
-        else{
+        } else {
             sendButton.setVisibility(Button.INVISIBLE);
             newGameButton.setText(R.string.new_game);
         }
 
     }
 
+
+    private void askForName()
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(SnakeMenu.this);
+        alertDialog.setTitle("Enter a Name");
+        //alertDialog.setMessage("Enter a name");
+
+        final EditText input = new EditText(this);
+        input.setInputType(TYPE_TEXT_VARIATION_PERSON_NAME);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+
+        //alertDialog.setIcon(R.drawable.key);
+
+
+        alertDialog.setPositiveButton("SEND",
+                new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        String name = input.getText().toString();
+                        Toast.makeText(getApplicationContext(),
+                                name, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        alertDialog.setNegativeButton("CANCEL",
+                new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(),
+                                "Geen naam ingevoerd", Toast.LENGTH_LONG).show();
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
+
+    }
 
 }
